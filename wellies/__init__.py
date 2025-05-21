ba96898a7cf67210f5fe1b0ab6fb56e8da1c2fec
Juan Colonese <Juan.Colonese@ecmwf.int>
2025-05-21 17:21:19 +0100

    move ecflow server into main library

--- wellies/__init__.py
+++ wellies/__init__.py
@@ -1,21 +1,22 @@
 # flake8: noqa
 
-from .config import (
-    concatenate_yaml_files,
-    get_parser,
-    get_user_globals,
-    overwrite_entries,
-    parse_submit_arguments,
-    get_config_files,
-    parse_yaml_files,
-    substitute_variables,
-)
-from .data import DeployDataFamily, StaticDataStore
+from .config import concatenate_yaml_files
+from .config import get_config_files
+from .config import get_parser
+from .config import get_user_globals
+from .config import overwrite_entries
+from .config import parse_submit_arguments
+from .config import parse_yaml_files
+from .config import substitute_variables
+from .data import DeployDataFamily
+from .data import StaticDataStore
 from .deployment import deploy_suite
+from .hosts import EcflowServer
 from .hosts import get_host
 from .log_archiving import ArchivedRepeatFamily
 from .tasks import EcfResourcesTask
-from .tools import DeployToolsFamily, ToolStore
+from .tools import DeployToolsFamily
+from .tools import ToolStore
 
 try:
     # NOTE: the `_version.py` file must not be present in the git repository
--- wellies/hosts.py
+++ wellies/hosts.py
@@ -1,9 +1,21 @@
 import os
 import shutil
+from dataclasses import dataclass
 
 import pyflow as pf
 
+from wellies.config import parse_submit_arguments
+
 local_host = ["localhost"]
+
+
+@dataclass
+class EcflowServer:
+    """Class to store ecflow server configuration"""
+
+    hostname: str
+    user: str
+    deploy_dir: str
 
 
 def get_host(
@@ -12,6 +24,8 @@ def get_host(
     ecflow_path: str = None,
     server_ecfvars: bool = False,
     extra_variables: dict = None,
+    job_out: str = None,
+    submit_arguments: dict = None,
     **kwargs,
 ) -> pf.Host:
     """
@@ -42,9 +56,16 @@ def get_host(
     if ecflow_path is None:
         ecflow_path = os.path.dirname(shutil.which("ecflow_client"))
 
+    kwargs.setdefault("log_directory", job_out)
+    options = submit_arguments or {}
+    submit_arguments, variables = parse_submit_arguments(options)
     if hostname in local_host:
         host = pf.LocalHost(
-            hostname, user=user, ecflow_path=ecflow_path, **kwargs
+            hostname,
+            user=user,
+            ecflow_path=ecflow_path,
+            submit_arguments=submit_arguments,
+            **kwargs,
         )
         print("Running on host: " + str(hostname))
     else:
@@ -56,8 +77,9 @@ def get_host(
             extra_variables=extra_variables,
             server_ecfvars=server_ecfvars,
             ecflow_path=ecflow_path,
+            submit_arguments=submit_arguments,
             **kwargs,
         )
         print("Submitting jobs using troika on host: " + str(hostname))
 
-    return host
+    return host, variables
