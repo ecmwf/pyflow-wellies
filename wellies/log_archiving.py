bc5f6debd87a8fe9452e99c146a021630168f4ab
Jenny Wong <jenny.wong@ecmwf.int>
2025-08-05 15:34:21 +0000

    Make log archiving compatible with group node

--- wellies/log_archiving.py
+++ wellies/log_archiving.py
@@ -85,7 +85,9 @@ class ArchivedRepeatFamily(pf.AnchorFamily):
             return
         script = textwrap.dedent(
             f"""
-            dir=$LOGS_BACKUP/$SUITE/$FAMILY
+            JOB=$(echo $JOB | sed -e "s:$ECF_HOME:$ECF_OUT:")
+            JOBDIR=$(echo ${{JOBOUT%%/*}})
+            dir=$(echo $JOBDIR | sed -e s:$ECF_OUT:$LOGS_BACKUP:)
             dir_old=${{dir}}.${self.repeat_attr.name}
             [[ -d $dir ]] && mv $dir $dir_old
             """
@@ -101,16 +103,18 @@ class ArchivedRepeatFamily(pf.AnchorFamily):
             return
         script = textwrap.dedent(
             f"""
-            dir=$LOGS_BACKUP/$SUITE/$FAMILY
-            dir_tar=$LOGS_BACKUP/$SUITE
+            JOB=$(echo $JOB | sed -e "s:$ECF_HOME:$ECF_OUT:")
+            JOBDIR=$(echo ${{JOBOUT%%/*}})
+            dir=$(echo $JOBDIR | sed -e s:$ECF_OUT:$LOGS_BACKUP:)
+            dir_tar=$(dirname $dir)
 
             if [[ -d $dir_tar ]]; then
                 cd $dir_tar
 
-                for log in $(ls -d ${{FAMILY}}.*); do
+                for log in $(ls -d ${{FAMILY1}}.*); do
                     REPEAT_TO_TAR=$(echo $log | awk -F'.' '{{print $NF}}')
                     if [[ $REPEAT_TO_TAR -lt ${self.repeat_attr.name} ]]; then
-                        TAR_FILE=${{FAMILY}}_${{REPEAT_TO_TAR}}.tar.gz
+                        TAR_FILE=${{FAMILY1}}_${{REPEAT_TO_TAR}}.tar.gz
                         tar -czvf $TAR_FILE $log
                         chmod 644 $TAR_FILE
                         ecp -p $TAR_FILE ${{LOGS_ARCHIVE}}/$TAR_FILE
